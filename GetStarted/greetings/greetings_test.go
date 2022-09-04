65486b382e85517f33905c58b1fb3a080cd400ac
kmannapperuma <kforkavan@live.com>
2022-09-04 22:54:07 +1000

    Unit test hello function

--- GetStarted/greetings/greetings_test.go
+++ GetStarted/greetings/greetings_test.go
@@ -2,9 +2,29 @@ package greetings
 
 import (
 	"regexp"
+	"testing"
 )
 
-func TestHelloName() {
+func TestHelloName(t *testing.T) {
 	name := "SomeName"
 	want := regexp.MustCompile(name)
+
+	message, err := Hello("SomeName")
+
+	if want.MatchString(message) && err == nil {
+		return
+	}
+
+	t.Fatalf(`Hello("SomeName") = %q, %v, want match for %#q, nil`, message, err, want)
+}
+
+func TestHelloEmpty(t *testing.T) {
+	message, err := Hello("")
+
+	//This is testing if the error handling works in the Hello function.
+	if message == "" && err != nil {
+		return
+	}
+
+	t.Fatalf(`Hello("") = %q, %v, want "", error`, message, err)
 }
